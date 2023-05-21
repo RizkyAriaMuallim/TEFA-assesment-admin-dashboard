a445e5d1c98d806335c1c06d1e497fe787b64d61
Rizky Aria Mu'allim <74603462+RizkyAriaMuallim@users.noreply.github.com>
2023-05-22 00:49:38 +0700

    just push

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,47 +1,46 @@
 import { Link } from "react-router-dom";
-import Product from "./Product";
-import User from "./Users";
+import { ReactComponent as LogoIcon } from '../images/sidebar/sidebar-logo.svg'
+import { ReactComponent as BoxIcon } from '../images/sidebar/box.svg'
+import { ReactComponent as PeopleIcon } from '../images/sidebar/people.svg'
+import BasePage from "../components/BasePage";
 
-export default function Root() {
+const Root = () => {
     return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <Link to={'/products'}>Products</Link>
-              </li>
-              <li>
-              <Link to={'/users'}>Users</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div id="detail"></div>
-      </>
-    );
-  }
+        <BasePage/>
+    )
+}
+
+const SideBar = () => {
+    return (
+        <aside className="absolute left-0 top-0 z-9999 flex h-screen flex-col overflow-y-hidden bg-slate-800">
+            {/* SIDEBAR HEADER */}
+            <div className="flex items-center gap-2 px-8 py-6 pb-11">
+                <LogoIcon/>
+            </div>
+
+            <div>
+                <nav className="px-4 pr-5">
+                    <div>
+                        <h3 className="mb-4 ml-4 text-sm text-slate-600 font-bold">PAGES</h3>
+                        <ul>
+                            <li className="text-white">
+                                <Link to={'/products'} className="group relative flex items-center gap-2.5 rounded-sm py-2 px-4 pr-32 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-slate-900">
+                                    <BoxIcon/>
+                                    Products
+                                </Link>
+                            </li>
+                            <li className="text-white">
+                                <Link to={'/users'} className="group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-gray-100 duration-300 ease-in-out hover:bg-slate-900">
+                                    <PeopleIcon/>
+                                    Users
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </aside>
+    )
+}
+
+export default Root;
